231a049b05c9efce2970847b902a8b71d8801ce4
punktkomma <87233344+punktkomma@users.noreply.github.com>
2024-03-10 18:06:43 +0100

    Update scene.ts

--- src/scene.ts
+++ src/scene.ts
@@ -1,4 +1,4 @@
-import GUI, { Controller } from 'lil-gui';
+import GUI from 'lil-gui';
 import {
   AmbientLight,
   AnimationAction,
@@ -6,7 +6,6 @@ import {
   AxesHelper,
   Clock,
   Color,
-  DirectionalLight,
   Fog,
   GridHelper,
   Group,
@@ -17,8 +16,6 @@ import {
   PCFSoftShadowMap,
   PerspectiveCamera,
   PlaneGeometry,
-  PointLight,
-  PointLightHelper,
   Scene,
   SkeletonHelper,
   WebGLRenderer,
@@ -42,22 +39,14 @@ let scene: Scene;
 let loadingManager: LoadingManager;
 let ambientLight: AmbientLight;
 let hemisphereLight: HemisphereLight;
-let pointLight: PointLight;
-let directionalLight: DirectionalLight;
 let camera: PerspectiveCamera;
 let cameraControls: OrbitControls;
 let axesHelper: AxesHelper;
-let pointLightHelper: PointLightHelper;
 let stats: Stats;
 let gui: GUI;
 
-const crossFadeControls: Controller[] = [];
 
-let idleAction: AnimationAction,
-  walkAction: AnimationAction,
-  runAction: AnimationAction;
-let idleWeight: number, walkWeight: number, runWeight: number;
-let actions: AnimationAction[];
+let walkAction: AnimationAction;
 let settings: any;
 
 let singleStepMode = false;
@@ -175,6 +164,25 @@ function init() {
     },
   ];
 
+    // ===== 👨🏻‍💼 LOADING MANAGER =====
+    {
+      loadingManager = new LoadingManager();
+  
+      loadingManager.onStart = () => {
+        console.log('loading started');
+      };
+      loadingManager.onProgress = (url, loaded, total) => {
+        console.log('loading in progress:');
+        console.log(`${url} -> ${loaded} / ${total}`);
+      };
+      loadingManager.onLoad = () => {
+        console.log('loaded!');
+      };
+      loadingManager.onError = () => {
+        console.log('❌ error while loading');
+      };
+    }
+
   // ===== 🖼️ CANVAS, RENDERER, & SCENE =====
   {
     canvas = document.querySelector(`canvas#${CANVAS_ID}`)!;
@@ -188,51 +196,15 @@ function init() {
     scene.fog = new Fog(0xa0a0a0, 10, 50);
   }
 
-  // ===== 👨🏻‍💼 LOADING MANAGER =====
-  {
-    loadingManager = new LoadingManager();
 
-    loadingManager.onStart = () => {
-      console.log('loading started');
-    };
-    loadingManager.onProgress = (url, loaded, total) => {
-      console.log('loading in progress:');
-      console.log(`${url} -> ${loaded} / ${total}`);
-    };
-    loadingManager.onLoad = () => {
-      console.log('loaded!');
-    };
-    loadingManager.onError = () => {
-      console.log('❌ error while loading');
-    };
-  }
 
   // ===== 💡 LIGHTS =====
   {
     ambientLight = new AmbientLight('white', 0.4);
-    directionalLight = new DirectionalLight(0xffffff, 3);
-    directionalLight.position.set(-3, 10, -10);
-    directionalLight.castShadow = true;
-    directionalLight.shadow.camera.top = 2;
-    directionalLight.shadow.camera.bottom = -2;
-    directionalLight.shadow.camera.left = -2;
-    directionalLight.shadow.camera.right = 2;
-    directionalLight.shadow.camera.near = 0.1;
-    directionalLight.shadow.camera.far = 40;
     hemisphereLight = new HemisphereLight(0xffffff, 0x8d8d8d, 3);
     hemisphereLight.position.set(0, 20, 0);
-    pointLight = new PointLight('#ffdca8', 1.2, 100);
-    pointLight.position.set(-2, 3, 3);
-    pointLight.castShadow = true;
-    pointLight.shadow.radius = 4;
-    pointLight.shadow.camera.near = 0.5;
-    pointLight.shadow.camera.far = 4000;
-    pointLight.shadow.mapSize.width = 2048;
-    pointLight.shadow.mapSize.height = 2048;
     scene.add(ambientLight);
-    scene.add(directionalLight);
     scene.add(hemisphereLight);
-    scene.add(pointLight);
   }
 
   // ===== 📦 OBJECTS =====
@@ -255,16 +227,8 @@ function init() {
       'models/blob.glb',
       (gltf) => {
         model = gltf.scene;
+        model.scale.set(0.2,0.2,0.2)
         scene.add(model);
-
-        model.traverse(
-          (child: { castShadow: boolean; receiveShadow: boolean }) => {
-            if (child instanceof Mesh) {
-              child.castShadow = true;
-              child.receiveShadow = true;
-            }
-          },
-        );
 
         skeleton = new SkeletonHelper(model);
         skeleton.visible = false;
@@ -274,7 +238,8 @@ function init() {
         console.log(animations);
         mixer = new AnimationMixer(model);
 
-        walkAction = mixer.clipAction(animations[0]);
+        walkAction = mixer.clipAction(animations[0]).play();
+        animate();
       },
       (xhr) => {
         console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`);
@@ -284,18 +249,7 @@ function init() {
         console.error(error);
       },
     );
-    const loader2 = new GLTFLoader(loadingManager);
-    loader2.load('models/Soldier.glb', (gltf) => {
-      const animations2 = gltf.animations;
-
-      idleAction = mixer.clipAction(animations2[0]);
-      runAction = mixer.clipAction(animations2[1]);
-
-      actions = [idleAction, walkAction, runAction];
-
-      activateAllActions();
-      animate();
-    });
+    
   }
 
   // ===== 🎥 CAMERA =====
@@ -331,9 +285,6 @@ function init() {
     axesHelper.visible = false;
     scene.add(axesHelper);
 
-    pointLightHelper = new PointLightHelper(pointLight, undefined, 'orange');
-    pointLightHelper.visible = false;
-    scene.add(pointLightHelper);
 
     const gridHelper = new GridHelper(20, 20, 'teal', 'darkgray');
     gridHelper.position.y = -0.01;
@@ -364,38 +315,13 @@ function init() {
     });
 
     const visibilityFolder = gui.addFolder('Visibility');
-    const activationFolder = gui.addFolder('Activation/Deactivation');
-    const pausingFolder = gui.addFolder('Pausing/Stepping');
-    const crossfadingFolder = gui.addFolder('Crossfading');
-    const blendWeightsFolder = gui.addFolder('Blend Weights');
     const speedFolder = gui.addFolder('General Speed');
     const musicFolder = gui.addFolder('Music');
 
     settings = {
       'show model': true,
       'show skeleton': false,
-      'deactivate all': deactivateAllActions,
-      'activate all': activateAllActions,
-      'pause/continue': pauseContinue,
-      'make single step': toSingleStepMode,
-      'modify step size': 0.05,
-      'from walk to idle': function () {
-        prepareCrossFade(walkAction, idleAction, 1.0);
-      },
-      'from idle to walk': function () {
-        prepareCrossFade(idleAction, walkAction, 0.5);
-      },
-      'from walk to run': function () {
-        prepareCrossFade(walkAction, runAction, 2.5);
-      },
-      'from run to walk': function () {
-        prepareCrossFade(runAction, walkAction, 5.0);
-      },
       'use default duration': true,
-      'set custom duration': 3.5,
-      'modify idle weight': 0.0,
-      'modify walk weight': 1.0,
-      'modify run weight': 0.0,
       'modify time scale': 1.0,
       'play music': false,
       'modify music volume': 0.5,
@@ -404,43 +330,9 @@ function init() {
     visibilityFolder.add(settings, 'show model').onChange(showModel);
     visibilityFolder.add(settings, 'show skeleton').onChange(showSkeleton);
 
-    activationFolder.add(settings, 'deactivate all');
-    activationFolder.add(settings, 'activate all');
 
-    pausingFolder.add(settings, 'pause/continue');
-    pausingFolder.add(settings, 'make single step');
-    pausingFolder.add(settings, 'modify step size', 0.01, 1, 0.01);
 
-    crossFadeControls.push(
-      crossfadingFolder.add(settings, 'from walk to idle'),
-    );
-    crossFadeControls.push(
-      crossfadingFolder.add(settings, 'from idle to walk'),
-    );
-    crossFadeControls.push(crossfadingFolder.add(settings, 'from walk to run'));
-    crossFadeControls.push(crossfadingFolder.add(settings, 'from run to walk'));
-
-    crossfadingFolder.add(settings, 'use default duration');
-    crossfadingFolder.add(settings, 'set custom duration', 0, 10, 0.01);
-
-    blendWeightsFolder
-      .add(settings, 'modify idle weight', 0.0, 1.0, 0.01)
-      .listen()
-      .onChange(function (weight: any) {
-        setWeight(idleAction, weight);
-      });
-    blendWeightsFolder
-      .add(settings, 'modify walk weight', 0.0, 1.0, 0.01)
-      .listen()
-      .onChange(function (weight: any) {
-        setWeight(walkAction, weight);
-      });
-    blendWeightsFolder
-      .add(settings, 'modify run weight', 0.0, 1.0, 0.01)
-      .listen()
-      .onChange(function (weight: any) {
-        setWeight(runAction, weight);
-      });
+   
 
     speedFolder
       .add(settings, 'modify time scale', 0.1, 2, 0.01)
@@ -452,20 +344,13 @@ function init() {
       .onChange(modifyVolume);
 
     visibilityFolder.open();
-    activationFolder.open();
-    pausingFolder.open();
-    crossfadingFolder.open();
-    blendWeightsFolder.open();
     speedFolder.open();
     musicFolder.open();
 
-    const lightsFolder = gui.addFolder('Lights');
-    lightsFolder.add(pointLight, 'visible').name('point light');
-    lightsFolder.add(ambientLight, 'visible').name('ambient light');
 
     const helpersFolder = gui.addFolder('Helpers');
     helpersFolder.add(axesHelper, 'visible').name('axes');
-    helpersFolder.add(pointLightHelper, 'visible').name('pointLight');
+   
 
     const cameraFolder = gui.addFolder('Camera');
     cameraFolder.add(cameraControls, 'autoRotate');
@@ -503,98 +388,7 @@ function modifyTimeScale(speed: number) {
   mixer.timeScale = speed;
 }
 
-function deactivateAllActions() {
-  actions.forEach(function (action) {
-    action.stop();
-  });
-}
 
-function activateAllActions() {
-  setWeight(idleAction, settings['modify idle weight']);
-  setWeight(walkAction, settings['modify walk weight']);
-  setWeight(runAction, settings['modify run weight']);
-  actions.forEach(function (action) {
-    action.play();
-  });
-}
-
-function pauseContinue() {
-  if (singleStepMode) {
-    singleStepMode = false;
-    unPauseAllActions();
-  } else {
-    if (idleAction.paused) {
-      unPauseAllActions();
-    } else {
-      pauseAllActions();
-    }
-  }
-}
-
-function pauseAllActions() {
-  actions.forEach(function (action) {
-    action.paused = true;
-  });
-}
-
-function unPauseAllActions() {
-  actions.forEach(function (action) {
-    action.paused = false;
-  });
-}
-
-function toSingleStepMode() {
-  unPauseAllActions();
-  singleStepMode = true;
-  sizeOfNextStep = settings['modify step size'];
-}
-
-function prepareCrossFade(
-  startAction: any,
-  endAction: any,
-  defaultDuration: any,
-) {
-  const duration = setCrossFadeDuration(defaultDuration);
-
-  singleStepMode = false;
-  unPauseAllActions();
-
-  if (startAction === walkAction) {
-    executeCrossFade(startAction, endAction, duration);
-  } else {
-    synchronizeCrossFade(startAction, endAction, duration);
-  }
-}
-
-function setCrossFadeDuration(defaultDuration: any) {
-  if (settings['use default duration']) {
-    return defaultDuration;
-  } else {
-    return settings['set custom duration'];
-  }
-}
-
-function synchronizeCrossFade(startAction: any, endAction: any, duration: any) {
-  mixer.addEventListener('loop', onLoopFinished);
-  function onLoopFinished(event: any) {
-    if (event.action === startAction) {
-      mixer.removeEventListener('loop', onLoopFinished);
-      executeCrossFade(startAction, endAction, duration);
-    }
-  }
-}
-
-function executeCrossFade(startAction: any, endAction: any, duration: any) {
-  setWeight(endAction, 1);
-  endAction.time = 0;
-  startAction.crossFadeTo(endAction, duration, true);
-}
-
-function setWeight(action: any, weight: any) {
-  action.enabled = true;
-  action.setEffectiveTimeScale(1);
-  action.setEffectiveWeight(weight);
-}
 
 function playMusic(play: boolean) {
   if (play) {
@@ -608,45 +402,12 @@ function modifyVolume(volume: number) {
   music.volume = volume;
 }
 
-function updateWeightSliders() {
-  settings['modify idle weight'] = idleWeight;
-  settings['modify walk weight'] = walkWeight;
-  settings['modify run weight'] = runWeight;
-}
 
-function updateCrossFadeControls() {
-  if (idleWeight === 1 && walkWeight === 0 && runWeight === 0) {
-    crossFadeControls[0].disable();
-    crossFadeControls[1].enable();
-    crossFadeControls[2].disable();
-    crossFadeControls[3].disable();
-  }
 
-  if (idleWeight === 0 && walkWeight === 1 && runWeight === 0) {
-    crossFadeControls[0].enable();
-    crossFadeControls[1].disable();
-    crossFadeControls[2].enable();
-    crossFadeControls[3].disable();
-  }
-
-  if (idleWeight === 0 && walkWeight === 0 && runWeight === 1) {
-    crossFadeControls[0].disable();
-    crossFadeControls[1].disable();
-    crossFadeControls[2].disable();
-    crossFadeControls[3].enable();
-  }
-}
 
 function animate() {
   requestAnimationFrame(animate);
 
-  idleWeight = idleAction.getEffectiveWeight();
-  walkWeight = walkAction.getEffectiveWeight();
-  runWeight = runAction.getEffectiveWeight();
-
-  updateWeightSliders();
-
-  updateCrossFadeControls();
 
   let mixerUpdateDelta = clock.getDelta();
 
